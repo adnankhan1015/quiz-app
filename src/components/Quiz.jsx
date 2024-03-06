413b595bf371e812809d0ced48b5d880d10ff72a
Adnan Khan <adnankhannad@gmail.com>
2024-03-06 23:59:11 +0500

    Add Header, Quiz Components and Questions file as input data

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,6 @@
+export default function Quiz() {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnwsers, setUserAnwsers] = useState([]);
+
+  return <p>Currently Active Questions</p>;
+}
