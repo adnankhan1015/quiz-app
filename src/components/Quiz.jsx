b35dc8dbad9a6ee20557bdba7e0f26e8dee79669
Adnan Khan <adnankhannad@gmail.com>
2024-03-08 00:09:01 +0500

    Display questions on the screen from the questions.js file

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  // const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnwsers, setUserAnwsers] = useState([]);
 
-  return <p>Currently Active Questions</p>;
+  // Derived State
+  const activeQuestionIndex = userAnwsers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnwsers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              {" "}
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>{" "}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
